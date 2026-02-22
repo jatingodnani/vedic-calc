ed880603710ed78e1fa7c4422fbba98248b5647c
jatin godnani <godnanijatin@gmail.com>
2026-02-23 01:57:00 +0530

    chore: remove tracked node_modules and dist from git index

--- dist/astrology/svgRenderer.d.ts
+++
@@ -1,9 +0,0 @@
-import { RasiChart } from './charts';
-export declare function generateNorthIndianChartSVG(chart: RasiChart, options?: {
-    showTable?: boolean;
-    width?: number;
-    height?: number;
-    layout?: 'row' | 'column';
-    tableWidth?: number;
-}): string;
-//# sourceMappingURL=svgRenderer.d.ts.map
